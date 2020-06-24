092faa8dd2efbe01e78c41058ad5f29973797331
Woshiajuana <zhigang.chen@owulia.com>
2020-06-24 10:12:26 +0800

    subcode code

--- cmd/config.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    entryDir: 'app', // 上传目录
-    inputDir: 'app/articles', // 遍历文章目录 可修改
-    outputDir: 'app/mocks', // mocks 数据生成目录
-    rootDir: 'ajuan', // 上传FTP 服务器 根目录 配置了这个之后就后续上传的时候就无需实时填写（不要斜杆开头结尾）
-    // 不要配置在此ftp密码
-}
